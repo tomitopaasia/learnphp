78070a87bc44068d3cd0ae7f17e2b1974fe17a71
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-04 13:06:41 +0300

    idk something new

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
 $test = 10;
-$test = 3.1;
-$test = "Hello World";
-$test = "õpilane";
-$test = "🥀";
-$test = true;
-$test = false;
-$test = null;
-$lol;
+$test = 10 + 2;
+$test = 10 - 2;
+$test = 10 * 2;
+$test = 10 / 2;
+$test -= 1;
+$test += 1;
+$test *= 1;
+$test /= 1;
+$test = 10 % 3;
+$test++;
+$test--;
+$test = (10 + (2 - 3) * 4) / 2;
+$test %= 2;
+$test **= 3;
+$test = 2 ** 4;
+$test = sqrt(16); 
 var_dump($test);
-var_dump($lol);
-$test = [1, 2, "three", true];
-var_dump($test);
-$test = new stdClass();
-$test->name = "John";
-var_dump($test);
-$num1 = 1000000000; // integer
-$num2 = 1000000000; // integer
-var_dump($num1 == $num2);
-?>
