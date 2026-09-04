947cb89ee32f33a32831c23a4e2a1e5535fe3d39
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-04 14:08:09 +0300

    Awesome sauce!

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
 <?php
-$test = true;
-$test = false;
-$test = true && false;
-$test = true || false;
-$test = !false;
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 < 10;
-$test = 10 == 10;
-$test = 10 <= 10;
-$test = 10 >= 10;
-$test = 10 != 10;
-$test = 10 !== 5;
-$test = 10 == '10';
-$test = 10 === '10';
-$test = true && false || (true && true);
+$test = [1, 2, 'word', true];
+var_dump($test[2]);
+$test[2] = 'another word';
 var_dump($test);
+$test[4] = 'new value';
+var_dump($test);
+$test[] = 'last value';
+var_dump($test);
+array_push($test, 'pushed value');
+var_dump($test);
+$test = [ 
+            'name' => 'Tomi', 
+            'age' => 20,
+            1,
+            3,
+            'word',
+            100 => 'new value',
+            5,  
+        ];
+var_dump($test['name']);
+var_dump($test);
+$test = [
+
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+var_dump($test[1][2]);
