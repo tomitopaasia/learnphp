5284fb58e59bfb2221a56b58d8cd8681ee6226f4
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-04 13:52:55 +0300

    Something new

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
-$test = 'hello';
-$test = 'hello' . 'world';
-$test .= '!';
-$name = 'Tomi';
-$age = 20;
-$test = "What's up $name? If you didn't already know i'm $age years old.";
-$test = "Hello\n\nWorld";
-$letter = 'a';
-$letter++;
-var_dump($letter);
+$test = true;
+$test = false;
+$test = true && false;
+$test = true || false;
+$test = !false;
+$test = 10 > 5;
+$test = 10 < 5;
+$test = 10 < 10;
+$test = 10 == 10;
+$test = 10 <= 10;
+$test = 10 >= 10;
+$test = 10 != 10;
+$test = 10 !== 5;
+$test = 10 == '10';
+$test = 10 === '10';
+$test = true && false || (true && true);
 var_dump($test);
