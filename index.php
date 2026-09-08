d5ac6425c4ff7530b5405ca99f39f11d6184914d
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-08 11:32:38 +0300

    fruits yay

--- index.php
+++ index.php
@@ -1,41 +1,40 @@
 <?php
-$num = 10;
-if($num > 5) {
-    var_dump('Bigger');
+
+for($i=0; $i<10; $i++){
+    var_dump($i);
+
 }
 
-if($num > 5) {
-    var_dump('Bigger');
-} else {
-    var_dump('Smaller');
+for($i=9; $i>=0; $i--){
+    var_dump($i);
 }
 
-if($num > 5) {
-    var_dump('Bigger');
-} elseif($num < 5) {
-    var_dump('Smaller');
-} else {
-    var_dump('Equal');
+for($i=2; $i=1_000_000; $i*=2){
+    var_dump($i);
 }
 
-$i = 1;
+$time = time();
+$n = 0;
+while($time+1 < time()) {
+    $n++;
 
-switch($i) {
-    case 1:
-        var_dump('yx');
-        break;
-    case 2:
-        var_dump('kaks');
-        break;
-    case 3:
-    case 4:
-        var_dump('kolm neli');
-        break;
-    case 5:
-        var_dump('viis');
-    case 6:
-        var_dump('kuus');
-        break;
-    default:
-        var_dump('dunno');
+}
+var_dump($n);
+
+while (false) {
+    var_dump('While');
+}
+
+do {
+    var_dump('do');
+} while(false);
+
+// while(true) {
+
+// }
+
+$fruits = ['Apple', 'Cherry', 'Banana'];
+
+foreach($fruits as $key=>$fruit) {
+    var_dump($fruit, $key);
 }
