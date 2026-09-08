5eafc2a24f5ec2af1198fb34493f5555a832b4ed
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-08 11:13:19 +0300

    Very kool!

--- index.php
+++ index.php
@@ -1,29 +1,41 @@
 <?php
-$test = [1, 2, 'word', true];
-var_dump($test[2]);
-$test[2] = 'another word';
-var_dump($test);
-$test[4] = 'new value';
-var_dump($test);
-$test[] = 'last value';
-var_dump($test);
-array_push($test, 'pushed value');
-var_dump($test);
-$test = [ 
-            'name' => 'Tomi', 
-            'age' => 20,
-            1,
-            3,
-            'word',
-            100 => 'new value',
-            5,  
-        ];
-var_dump($test['name']);
-var_dump($test);
-$test = [
+$num = 10;
+if($num > 5) {
+    var_dump('Bigger');
+}
 
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-];
-var_dump($test[1][2]);
+if($num > 5) {
+    var_dump('Bigger');
+} else {
+    var_dump('Smaller');
+}
+
+if($num > 5) {
+    var_dump('Bigger');
+} elseif($num < 5) {
+    var_dump('Smaller');
+} else {
+    var_dump('Equal');
+}
+
+$i = 1;
+
+switch($i) {
+    case 1:
+        var_dump('yx');
+        break;
+    case 2:
+        var_dump('kaks');
+        break;
+    case 3:
+    case 4:
+        var_dump('kolm neli');
+        break;
+    case 5:
+        var_dump('viis');
+    case 6:
+        var_dump('kuus');
+        break;
+    default:
+        var_dump('dunno');
+}
