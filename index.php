99702f6fd0a1dfda5b24916cd7ce56fcc87d79ba
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-09 13:51:29 +0300

    next commit!

--- index.php
+++ index.php
@@ -1,51 +1,23 @@
 <?php
 
-class Box {
-    public $width;
-    private $height;
-    protected $length;
-    public bool $isOpen = false;
-    public bool $hasBeenOpened = false;
+class cat {
+    use MakesSounds;
+}
 
-    public function open() {
-        $this->isOpen = true;
-        $this->hasBeenOpened = true;
-    }
+class dog {
+    use HasSmell, makeSound;
+}
 
-    public function close() {
-        $this->isOpen = false;
-    }
-
-    public function volume() {
-        return $this->height * $this->length * $this->width;
-    }
-
-    public function test() {
-        var_dump($this->length);
-    }
-
-    public function setHeight(int $height) {
-        if($height > 0) {
-            $this->height = $height;
-        }
-    }
-
-    public function getHeight() {
-        return $this->height;
+trait hasSmell {
+    public $smell;
+    public function sniff() {
+        return 'Smells like ' . $this->smell;
     }
 }
 
-class MetalBox extends Box {
-    public $weight;
-
-    public function mass(){
-        return $this->volume() * $this->weight;
-    }
-    public function test2() {
-        var_dump($this->height);
+trait MakeSound {
+    public $sound;
+    public function noise() {
+        return 'Makes a ' . $this->sound . ' sound';
     }
 }
-
-$metal1 = new MetalBox();
-$metal1->test2();
-var_dump($metal1);
