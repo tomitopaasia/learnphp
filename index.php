d242a6c4cf015e8ab2a5d8a0217c6ecca2ecde3e
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-11 13:47:36 +0300

    yupieee!

--- index.php
+++ index.php
@@ -1,55 +1,28 @@
 <?php
 
 class Box {
-
+    public static $count = 0;
     public function __construct(private $w, private $h, private $l) {
-        var_dump('Box was created!');
+        self::$count++;
     }
 
     public function volume(){
         return $this->w * $this->h * $this->l;
     }
 
-    public function __set($name, $value) {
-        var_dump($name, $value);
+    public static function me() {
+        var_dump(self::class);
+        var_dump(static::class);
+        
     }
-
-    public function __get($name) {
-        var_dump($name);
-        return 'YOLO';
-    }
-
-    public function __invoke() {
-        var_dump('Im a function');
-    }
-    
-    public function __call($name, $args) {
-        var_dump($name, $args);
-    }
-
-    public function __toString() {
-        return "Im a box with W: $this->w H: $this->h L: $this->l";
-    }
-
-    public function __destruct() {
-        var_dump('Box was destroyed!');
-    }
-    
 }
 
-function lol(){
-    $box4 = new Box(4, 4, 5);
+class MetalBox extends Box {
+
 }
 
-lol();
-
-$box1 = new Box(1,2,3);
-$box1->color = 'hello';
-$box1();
-$box1->hello(1, 'aasda', 'azczxc');
-var_dump($box1->lolollol);
-$box1 = 1;
-$box2 = new Box(4,5,6);
-var_dump($box1, $box2);
-$box3 = clone $box2;
-echo $box1;
+Box::$count = 1;
+Box::$count = 2;
+Box::me();
+MetalBox::me();
+var_dump(Box::$count, Box::$count);
