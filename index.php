a68405fc0eaffa9e1067586a47aa1c2987aab435
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-11 13:29:44 +0300

    aasda

--- index.php
+++ index.php
@@ -3,26 +3,53 @@
 class Box {
 
     public function __construct(private $w, private $h, private $l) {
-        var_dump('Box has been created');
+        var_dump('Box was created!');
     }
 
     public function volume(){
         return $this->w * $this->h * $this->l;
     }
 
+    public function __set($name, $value) {
+        var_dump($name, $value);
+    }
+
+    public function __get($name) {
+        var_dump($name);
+        return 'YOLO';
+    }
+
+    public function __invoke() {
+        var_dump('Im a function');
+    }
+    
+    public function __call($name, $args) {
+        var_dump($name, $args);
+    }
+
     public function __toString() {
-        return "I'm a box with W: $this->w H: $this->h L: $this->l";
+        return "Im a box with W: $this->w H: $this->h L: $this->l";
     }
 
     public function __destruct() {
-        var_dump('Box was destroyed');
+        var_dump('Box was destroyed!');
     }
+    
 }
 
-$box1 = new Box(1, 2, 3);
-$box4 = $box1;
+function lol(){
+    $box4 = new Box(4, 4, 5);
+}
+
+lol();
+
+$box1 = new Box(1,2,3);
+$box1->color = 'hello';
+$box1();
+$box1->hello(1, 'aasda', 'azczxc');
+var_dump($box1->lolollol);
 $box1 = 1;
-$box2 = new Box(4, 5, 6);
+$box2 = new Box(4,5,6);
 var_dump($box1, $box2);
 $box3 = clone $box2;
 echo $box1;
