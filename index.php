a789da3eaf2bdde45b2454ea1bad27942bab5e7d
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-11 13:13:59 +0300

    construct, tostring, destruct

--- index.php
+++ index.php
@@ -1,23 +1,28 @@
 <?php
 
-class cat {
-    use MakesSounds;
-}
+class Box {
 
-class dog {
-    use HasSmell, makeSound;
-}
+    public function __construct(private $w, private $h, private $l) {
+        var_dump('Box has been created');
+    }
 
-trait hasSmell {
-    public $smell;
-    public function sniff() {
-        return 'Smells like ' . $this->smell;
+    public function volume(){
+        return $this->w * $this->h * $this->l;
+    }
+
+    public function __toString() {
+        return "I'm a box with W: $this->w H: $this->h L: $this->l";
+    }
+
+    public function __destruct() {
+        var_dump('Box was destroyed');
     }
 }
 
-trait MakeSound {
-    public $sound;
-    public function noise() {
-        return 'Makes a ' . $this->sound . ' sound';
-    }
-}
+$box1 = new Box(1, 2, 3);
+$box4 = $box1;
+$box1 = 1;
+$box2 = new Box(4, 5, 6);
+var_dump($box1, $box2);
+$box3 = clone $box2;
+echo $box1;
