92a277caa5d2670595a4f2f195248c02da3a2065
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-11 14:14:01 +0300

    mdea

--- index.php
+++ index.php
@@ -1,28 +1,43 @@
 <?php
 
-class Box {
-    public static $count = 0;
-    public function __construct(private $w, private $h, private $l) {
-        self::$count++;
-    }
+// library
 
-    public function volume(){
-        return $this->w * $this->h * $this->l;
-    }
-
-    public static function me() {
-        var_dump(self::class);
-        var_dump(static::class);
-        
+class Job {
+    public function task(Logger $logger) {
+        for($i=0;$i<10;$i++){
+            //some work is done (video proccessing or what ever)
+            $logger->log("Task $i was done!");
+        }
     }
 }
 
-class MetalBox extends Box {
-
+class ConsoleLogger implements Logger {
+    public function log($message) {
+        echo "$message\n";
+    }
 }
 
-Box::$count = 1;
-Box::$count = 2;
-Box::me();
-MetalBox::me();
-var_dump(Box::$count, Box::$count);
+class NothingLogger implements Logger {
+    public function log($message) {
+
+    }
+}
+
+interface Logger {
+    public function log($message);
+}
+
+
+// user code
+
+class FileLogger implements Logger {
+    public function log($message) {
+        $file = fopen('log.txt', 'a');
+        fwrite($file, "$message\n");
+        fclose($file);
+    }
+}
+
+$job = new Job();
+$logger = new FileLogger();
+$job->task($logger);
