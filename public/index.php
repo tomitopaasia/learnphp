5fae06bf0e9556b01ad62e73ed2f041334ce764c
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-16 13:53:23 +0300

    moving logic to controllers

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 }
 
+
 spl_autoload_register(function ($class){
     $class = substr($class,4);
     $class = str_replace('\\', '/', $class);
@@ -15,7 +16,14 @@ require __DIR__ . '/../routes.php';
 $router = new App\Router($_SERVER['REQUEST_URI']);
 $match = $router->match();
 if($match) {
-    call_user_func($match['action']);
+    if(is_callable($match['action'])) {
+        call_user_func($match['action']);
+    } else if(is_array($match['action'])) {
+        $class = $match['action'][0];
+        $controller = new $class();
+        $method = $match['action'][1];
+        $controller->$method();
+    }
 } else {
     echo 404;
 }
