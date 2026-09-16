bcb0ce3fdf5d807c7222feecf68731bfadd85c08
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-16 12:55:43 +0300

    Add Router Logic

---
+++ views/us.php
@@ -0,0 +1,13 @@
+<?php include __DIR__ . '/partials/header.php'; ?>
+
+    <main class="container">
+      <div class="row g-5">
+        <div class="col-md-8">
+          <?php include __DIR__ . '/partials/posts.php'; ?>
+        </div>
+        <div class="col-md-4">
+          <?php include __DIR__ . '/partials/sidebar.php'; ?>
+        </div>
+      </div>
+    </main>
+  <?php include __DIR__ . '/partials/footer.php'; ?>
