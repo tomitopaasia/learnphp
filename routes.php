3bcf338c079b83d5efc4b0b4c53e236b54ba9fc3
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-21 14:46:53 +0300

    Add forms page

--- routes.php
+++ routes.php
@@ -6,3 +6,5 @@ use App\Router;
 Router::addRoute('/', [PublicController::class, 'index']);
 
 Router::addRoute('/us', [PublicController::class, 'us']);
+
+Router::addRoute('/forms', [PublicController::class, 'forms']);
