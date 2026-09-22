98a6ccbaa8d9349372203b02a724db08ae4609b3
Tomi Topaasia <tomitopaasiay@gmail.com>
2026-09-22 11:42:05 +0300

    Add db class to handle database connection

--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -2,38 +2,17 @@
 
 namespace App\Controllers;
 
+use App\DB;
+
 class PublicController
 {
     public function index()
     {
-        $title = 'World';
-        $posts = [
-            [
-                'title' => 'Some world title 1',
-                'content' => 'Some world content 1',
-                'date' => 'January 1, 2021',
-                'author' => 'Pets',
-            ],
-            [
-                'title' => 'Some world title 2',
-                'content' => 'Some world content 2',
-                'date' => 'January 2, 2021',
-                'author' => 'Juss',
-            ],
-            [
-                'title' => 'Some world title 3',
-                'content' => 'Some world content 3',
-                'date' => 'January 3, 2021',
-                'author' => 'Alex',
-            ],
-            [
-                'title' => 'Some world title 4',
-                'content' => 'Some world content 4',
-                'date' => 'January 4, 2021',
-                'author' => 'Manivald',
-            ],
-        ];
-        view('index', compact('title', 'posts'));
+        $db = new DB();
+        $articles = $db->all();
+        dump($articles);
+        // $title = 'World';
+        // view('index', compact('title', 'posts'));
     }
 
     public function us()
@@ -67,12 +46,14 @@ class PublicController
         ];
         view('us', compact('title', 'posts'));
     }
-    
-    public function forms() {
+
+    public function forms()
+    {
         view('forms');
     }
 
-    public function answer() {
+    public function answer()
+    {
         dump($_GET);
         dump($_POST);
     }
